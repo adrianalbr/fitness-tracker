0916a484bc76a0d6afebba5c850ce3f5a4ffd863
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2021-01-05 20:34:25 -0500

    finished routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -16,8 +16,35 @@ module.exports = (app) => {
                 console.log("error GET /api/workouts: ", err);
                 res.json(err);
             });
+    });
 
+    //create new workout
+    app.post('/api/workouts', ({body}, res) => {
+        console.log ("request /api/workouts: ", body);
+        db.Workout.create(body)
+            .then(dbWorkout => {
+                console.log("new workout has been created: ", dbWorkout)
+                res.json(dbWorkout);
+            })
+            .catch(({message}) => {
+                console.log("error port/api/workouts", message);
+            });
+     });
 
-
-    })
-}
+     // add new exercise to Workout
+    app.put('/api/workouts/:id', (req, res) => {
+        db.Workout.findByIdAndUpdate(
+            { _id: req.params.id },
+            { $push: { exercises: req.body } },
+            function (err, result) {
+                if (err) {
+                    console.log("Error: ", err);
+                    res.send(err);
+                } else {
+                    console.log("Updated dbWorkout: ", result);
+                    res.send(result);
+                }
+            }
+        );
+    });
+};
